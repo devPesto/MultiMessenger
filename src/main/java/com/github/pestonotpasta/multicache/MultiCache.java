29b0b8f733f05473ccf5771917ba53730f35a495
_Pesto <pestonotpasta@gmail.com>
2022-05-02 18:31:19 +0200

    Adds logging to membership listener

--- src/main/java/com/github/pestonotpasta/multicache/MultiCache.java
+++ src/main/java/com/github/pestonotpasta/multicache/MultiCache.java
@@ -9,6 +9,9 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
+import java.util.Set;
+import java.util.UUID;
+import java.util.logging.Logger;
 
 public final class MultiCache extends JavaPlugin {
     private final File configFile = new File(getDataFolder(), "config.yml");
@@ -39,11 +42,33 @@ public final class MultiCache extends JavaPlugin {
         this.hazelcast = Hazelcast.newHazelcastInstance(hazelConf);
     }
 
-    public MultiCache getInstance(){
-        return instance;
+    private static void logMembers() {
+        Set<Member> members = hazelcast.getCluster().getMembers();
+        log.info("--------------------------------------------------------------");
+        log.info("Members:");
+        int i = 1;
+        for (Member member : members) {
+            String host = member.getAddress().getHost();
+            int port = member.getAddress().getPort();
+            UUID id = member.getUuid();
+            log.info(String.format("%d. [%s:%d] - %s", i, host, port, id.toString()));
+            i++;
+        }
+        log.info("--------------------------------------------------------------");
     }
 
-    public HazelcastInstance getHazelcast(){
-        return hazelcast;
+    private static class HazelcastListener implements MembershipListener {
+
+        @Override
+        public void memberAdded(MembershipEvent membershipEvent) {
+            log.info("Cluster member added...");
+            logMembers();
+        }
+
+        @Override
+        public void memberRemoved(MembershipEvent membershipEvent) {
+            log.info("Cluster member removed...");
+            logMembers();
+        }
     }
 }
