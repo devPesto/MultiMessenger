2301a865bc4b73b5f518ea0e7d29d72cfb0a83a6
_Pesto <pestonotpasta@gmail.com>
2022-05-02 13:02:15 +0200

    Optimised imports

--- src/main/java/com/github/pestonotpasta/multicache/MultiCache.java
+++ src/main/java/com/github/pestonotpasta/multicache/MultiCache.java
@@ -9,7 +9,6 @@ import org.bukkit.plugin.java.JavaPlugin;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Path;
 
 public final class MultiCache extends JavaPlugin {
     private final File configFile = new File(getDataFolder(), "config.yml");
